c0e85a464c9f60d5c312bf7c2363ad70a36125f0
Google-X <i.code.like.crazy.everyday@gmail.com>
2020-05-26 10:35:14 +0800

    [Congkak] Steps generator @ 260520 10:34

---
+++ src/Congkak/Generator.java
@@ -0,0 +1,29 @@
+
+package Congkak;
+
+/*
+    [7][7][7][7][7][7][7]
+[p1]                     [p2]
+    [7][7][7][7][7][7][7]
+
+INDEX
+    [ 8][ 9][10][11][12][13][14]
+[ 7]                            [15]
+    [ 6][ 5][ 4][ 3][ 2][ 1][ 0]
+
+*/
+
+public class Generator {
+    public static void main(String[] args) {
+        
+        int setHole = 7;
+        int setMarbleEachHole = 7;
+        int p1Store = 0;
+        int p2Store = 0;
+        
+        int[] house = new int[(7*2)+2];
+        
+        
+        
+    }
+}
